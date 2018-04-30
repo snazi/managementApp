ab793235b7f0397920fce37f3081dda577532b42
Angelo Amadora <amadora.angelo@gmail.com>
2018-04-30 17:07:13 +0800

    i didnt understand

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from './reducers';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBR4FTGiOcTZ5OBNi6thE0KhCDR3PaOtQM',
+      authDomain: 'manager-2328b.firebaseapp.com',
+      databaseURL: 'https://manager-2328b.firebaseio.com',
+      storageBucket: 'manager-2328b.appspot.com',
+      messagingSenderId: '193075244299'
+    };
 
-    componentWillMount() {
-        const config = {
-            apiKey: "AIzaSyBoWPCG_5957t5RK60oGa6yRCcyaYJ7rc8",
-            authDomain: "management-project-224b2.firebaseapp.com",
-            databaseURL: "https://management-project-224b2.firebaseio.com",
-            projectId: "management-project-224b2",
-            storageBucket: "management-project-224b2.appspot.com",
-            messagingSenderId: "324787196237"
-        };
+    firebase.initializeApp(config);
+  }
 
-        firebase.initializeApp(config);
-    }
-
-    render() {
-        return (
-            <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        ha putangina
-                    </Text>
-                </View>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
+    );
+  }
 }
 
 export default App;
