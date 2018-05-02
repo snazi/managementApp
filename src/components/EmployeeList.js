dfbaeb863d4ea23e0cba1ccdb54c930fcdf3b333
Angelo Amadora <amadora.angelo@gmail.com>
2018-05-02 19:43:58 +0800

    pseudo navigation

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class EmployeeList extends Component {
+    render() {
+        return(
+            <View>
+                <Text> EMPLOYEE LIST </Text>
+                <Text> EMPLOYEE LIST </Text>
+                <Text> EMPLOYEE LIST </Text>
+                <Text> EMPLOYEE LIST </Text>
+                <Text> EMPLOYEE LIST </Text>
+                <Text> EMPLOYEE LIST </Text>
+                <Text> EMPLOYEE LIST </Text>
+                <Text> EMPLOYEE LIST </Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeList;
