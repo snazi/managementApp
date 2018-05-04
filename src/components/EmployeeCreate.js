1d3e654aa61ae713e4de0f5ad65ce488471d8784
Angelo Amadora <amadora.angelo@gmail.com>
2018-05-04 17:58:34 +0800

    can make an employee

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -2,9 +2,17 @@ import React, {Component} from 'react';
 import {View, Text, Picker} from 'react-native';
 import {Card, CardSection, Input, Button} from './common';
 import {connect} from 'react-redux';
-import {employeeUpdate} from '../actions';
+import {employeeUpdate, employeeCreate} from '../actions';
+
 
 class EmployeeCreate extends Component {
+
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    }
+
     render(){
         return(
             <Card>
@@ -44,7 +52,7 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Create
                     </Button>
                 </CardSection>
@@ -66,4 +74,9 @@ const mapStateToProps = (state) => {
     return {name, phone, shift};
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { 
+    
+    employeeUpdate,
+    employeeCreate
+
+})(EmployeeCreate);
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,19 +1,21 @@
 import {
-    EMPLOYEE_UPDATE
-} from '../actions/types';
-
-const INITIAL_STATE ={
+    EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE
+  } from '../actions/types';
+  
+  const INITIAL_STATE = {
     name: '',
     phone: '',
     shift: ''
-};
-
-export default (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        case EMPLOYEE_UPDATE:
-            //action.payload === { prop:'name', value: 'jane' }
-            return{ ...state, [action.payload.prop]: action.payload.value };
-        default:
-            return state;
+  };
+  
+  export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case EMPLOYEE_UPDATE:
+        return { ...state, [action.payload.prop]: action.payload.value };
+      case EMPLOYEE_CREATE:
+        return INITIAL_STATE;
+      default:
+        return state;
     }
-};
+  };
